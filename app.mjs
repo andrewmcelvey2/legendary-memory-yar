5b2880aa8614a571112cdf5aa86566248602a439
McElvey <hmcelvey@una.edu>
2026-02-19 13:08:28 -0600

    update the dev branch

--- app.mjs
+++ app.mjs
@@ -3,7 +3,11 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
+<<<<<<< HEAD
   res.send('Hello World | Final Test from dev branch <a href="https://github.com/andrewmcelvey2/legendary-memory-yar" target="blank">legendary-memory-yar</a>')
+=======
+  res.send('Hello World | Final Test to ci/cd <a href="https://github.com/andrewmcelvey2/legendary-memory-yar" target="blank">legendary-memory-yar</a>')
+>>>>>>> origin/main
 })
 
 app.listen(3000, () => {
